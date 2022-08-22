a31617999d8913dee1d5b9684e660160ced27c27
iurkenty <vadaturschii.iurii@gmail.com>
2022-08-22 16:04:32 -0700

    restructured the directory tree; added sysadmin users in IAM

--- gogreen/IAM/main.tf
+++ gogreen/IAM/main.tf
@@ -1,3 +1,22 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
+}
+//noinspection MissingModule
+module "SysAdmin" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  name = var.sysadmin[count.index]
+  count = length(var.sysadmin)
+
+  create_iam_user_login_profile = false
+  create_iam_access_key = false
+}
+
+//noinspection MissingModule
+module "DBadmin" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  name = var.dbadmin[count.index]
+  count = length(var.dbadmin)
+
 }
---
+++ gogreen/IAM/variables.tf
@@ -0,0 +1,14 @@
+variable "sysadmin" {
+  type = list
+  default = ["sysadmin1", "sysadmin2"]
+}
+
+variable "dbadmin" {
+  type = list
+  default = ["dbadmin1", "dbadmin2"]
+}
+
+variable "monitoruser" {
+  type = list
+  default = ["monitoruser1", "monitoruser2", "monitoruser3"]
+}
