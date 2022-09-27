bd6a32a0a083928d11226883cbcae8b0a0f3193d
iurkenty <vadaturschii.iurii@gmail.com>
2022-09-27 12:48:47 -0700

    testing the pipeline, new ec2

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Launch resources in different regions use alias = "name"
+/*
+provider "aws" {
+  region     =  "us-west-1"
+}
+provider "aws" {
+  alias      =  "aws02"
+  region     =  "ap-south-1"
+*/
+
+# Launch resources in a different account use profile = "name"
+# Account credentials must be stored in the providers(aws) config file
+/*
+provider "aws" {
+  alias      =  "aws02"
+  region     =  "ap-south-1"
+  profile    =  "account02"
+}
+*/
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "EC2-test" {
+  ami = "ami-08e2d37b6a0129927"
+  instance_type = "t2.micro"
+}
+/*
+module "ASG" {
+  source = "github.com/terraform-aws-modules/terraform-aws-autoscaling"
+  name   = ""
+}
+*/
